06e124bbbb3c12ec54390f63ca50deb6459d504b
PBaloyi449 <pbaloyi449@gmail.com>
2024-06-07 09:56:13 +0200

    Updating the code

--- index.jsx
+++ index.jsx
@@ -1,11 +1,11 @@
-//import React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Vans from "./pages/Vans"
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Vans from './pages/Vans';
 
-import "./server"
+import './server';
 
 function App() {
   return (
@@ -15,17 +15,15 @@ function App() {
         <Link to="/vans">Vans</Link>
         <nav>
           <Link to="/about">About</Link>
-          <Route path="/vans" element={<Vans />} />
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-ReactDOM
-  .createRoot(document.getElementById('root'))
-  .render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
